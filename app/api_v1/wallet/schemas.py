2d130ace381f03202cd7f98f515ed0733e54814f
Botalov Sergey <89090168690@mail.ru>
2025-07-21 13:41:20 +0500

    Обновил эндпоитн полуения баланса кошелька

--- app/api_v1/wallet/schemas.py
+++ app/api_v1/wallet/schemas.py
@@ -1,11 +1,19 @@
+from decimal import Decimal
 from uuid import UUID
 
 from pydantic import BaseModel
 
 
-class WalletResponse(BaseModel):
+class WalletBase(BaseModel):
     id: UUID
+
+
+class WalletCreateResponse(WalletBase):
     email: str
 
     class Config:
         from_attributes = True
+
+
+class WalletResponse(WalletBase):
+    balance: Decimal
--- app/api_v1/wallet/views.py
+++ app/api_v1/wallet/views.py
@@ -6,7 +6,7 @@ from pydantic import EmailStr
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.api_v1.wallet.schemas import WalletResponse
+from app.api_v1.wallet.schemas import WalletCreateResponse, WalletResponse
 from app.core import db_helper, logger
 from app.crud.base import test_connection
 from app.crud.wallet import create_wallet_by_email, get_wallet_by_id
@@ -26,26 +26,44 @@ async def health_check(
 async def perform_operation(): ...
 
 
-@router.get("/{wallet_id}")
+@router.get("/{wallet_id}", response_model=WalletResponse)
 async def get_wallet(
     wallet_id: uuid.UUID,
     session: Annotated[AsyncSession, Depends(db_helper.sesion_getter)],
-):
+) -> WalletResponse:
+    """Получает информацию о кошельке по его UUID.
+
+    Args:
+        wallet_id: UUID кошелька.
+        session: Асинхронная сессия SQLAlchemy.
+
+    Returns:
+        WalletResponse: Данные кошелька (id, balance, email).
+
+    Raises:
+        HTTPException:
+            - 404: Если кошелёк не найден.
+    """
     wallet = await get_wallet_by_id(session, wallet_id)
-    if wallet:
-        return {"wallet": wallet}
-    return {"message": "Not found"}
+    if not wallet:
+        logger.debug(f"Кошелёк с ID {wallet_id} не найден")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Кошелёк не найден",
+        )
+    logger.info(f"Кошелёк с ID {wallet_id} успешно получен")
+    return WalletResponse(id=wallet.id, balance=wallet.balance)
 
 
 @router.post(
     "/create-wallet",
     status_code=status.HTTP_201_CREATED,
-    response_model=WalletResponse,
+    response_model=WalletCreateResponse,
 )
 async def create_wallet(
     email: EmailStr,
     session: Annotated[AsyncSession, Depends(db_helper.sesion_getter)],
-) -> WalletResponse:
+) -> WalletCreateResponse:
     """Создаёт новый кошелёк с указанным email.
 
     Args:
@@ -69,7 +87,7 @@ async def create_wallet(
                 detail="Кошелёк с таким email уже существует",
             )
         logger.info(f"create_wallet: Кошелёк создан для email {email}")
-        return WalletResponse(id=wallet.id, email=wallet.email)
+        return WalletCreateResponse(id=wallet.id, email=wallet.email)
     except ValueError:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
