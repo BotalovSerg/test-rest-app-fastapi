e224aa122d1ff917e44c3843c701f8d58857026d
Botalov Sergey <89090168690@mail.ru>
2025-07-22 23:05:14 +0500

    added tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,35 +1,34 @@
 import httpx
-import pytest_asyncio
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
+import pytest
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
+from app.core import db_helper
+from app.models import Operation, Wallet
 from main import app
 
 
-@pytest_asyncio.fixture
-async def client():
+@pytest.fixture
+async def session():
+    engine = create_async_engine("sqlite+aiosqlite:///:memory:", echo=False)
+    async with engine.begin() as conn:
+        await conn.run_sync(Wallet.metadata.create_all)
+        await conn.run_sync(Operation.metadata.create_all)
+    session_local = async_sessionmaker(engine, expire_on_commit=False)
+    async with session_local() as session:
+        yield session
+        await session.rollback()
+    await engine.dispose()
+
+
+@pytest.fixture
+async def client(session: AsyncSession):
+    async def override_session_getter():
+        yield session
+
+    app.dependency_overrides[db_helper.sesion_getter] = override_session_getter
     async with httpx.AsyncClient(
         transport=httpx.ASGITransport(app=app),
         base_url="http://test",
     ) as ac:
         yield ac
-
-
-TEST_DATABASE_URL = "postgresql+asyncpg://admin:123456@localhost:5433/db_app"
-engine = create_async_engine(TEST_DATABASE_URL, echo=False)
-
-TestingSessionLocal = sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False,
-)
-
-
-@pytest_asyncio.fixture(scope="module")
-async def db_session():
-
-    session = TestingSessionLocal()
-    yield session
-
-    await session.rollback()
-    await session.close()
+    app.dependency_overrides.clear()
--- tests/test_wallets.py
+++ tests/test_wallets.py
@@ -8,16 +8,25 @@ from app.models import Wallet
 
 
 @pytest.mark.asyncio
-async def test_get_wallet_success(client, db_session: AsyncSession):
+async def test_ping_pong(client):
+    response = await client.get("/api/v1/wallets/health_check")
+
+    assert response.status_code == 200
+    data = response.json()
+    assert data == {"messege": "OK"}
+
+
+@pytest.mark.asyncio
+async def test_get_wallet_success(client, session: AsyncSession):
     wallet = Wallet(email="test@example.com", balance=Decimal("100.50"))
-    db_session.add(wallet)
-    await db_session.commit()
+    session.add(wallet)
+    await session.commit()
 
     response = await client.get(f"/api/v1/wallets/{wallet.id}")
     assert response.status_code == 200
     data = response.json()
     assert data["id"] == str(wallet.id)
-    assert data["balance"] == "100.5000"
+    assert data["balance"] == "100.50"
 
 
 @pytest.mark.asyncio
@@ -33,3 +42,90 @@ async def test_get_wallet_invalid_uuid(client):
     response = await client.get("/api/v1/wallets/invalid_uuid")
     assert response.status_code == 422
     assert "Input should be a valid UUID, invalid character" in response.json()["detail"][0]["msg"]
+
+
+@pytest.mark.parametrize(
+    "amount",
+    [("5"), ("10")],
+)
+@pytest.mark.asyncio
+async def test_create_operation_success_deposit(
+    client,
+    session: AsyncSession,
+    amount: str,
+):
+    wallet = Wallet(email="test@example.com", balance=Decimal("0"))
+    session.add(wallet)
+    await session.commit()
+
+    amount = Decimal(amount)
+    new_balance = wallet.balance + amount
+
+    response = await client.post(
+        f"/api/v1/wallets/{wallet.id}/operation",
+        json={"operation_type": "DEPOSIT", "amount": str(amount)},
+    )
+    assert response.status_code == 200
+    assert wallet.balance == new_balance
+    data = response.json()
+    assert data.get("wallet_id") == str(wallet.id)
+    assert data.get("operation_type") == "DEPOSIT"
+
+
+@pytest.mark.asyncio
+async def test_create_operation_not_found_wallet(client):
+    response = await client.post(
+        f"/api/v1/wallets/{uuid4()}/operation",
+        json={"operation_type": "DEPOSIT", "amount": "50"},
+    )
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Кошелёк не найден"}
+
+
+@pytest.mark.parametrize(
+    "amount",
+    [("9"), ("10")],
+)
+@pytest.mark.asyncio
+async def test_create_operation_withraw_succsess(
+    client,
+    session: AsyncSession,
+    amount: str,
+):
+    wallet = Wallet(email="test@example.com", balance=Decimal("10"))
+    session.add(wallet)
+    await session.commit()
+
+    new_balance = wallet.balance - Decimal(amount)
+
+    response = await client.post(
+        f"/api/v1/wallets/{wallet.id}/operation",
+        json={"operation_type": "WITHDRAW", "amount": amount},
+    )
+    assert response.status_code == 200
+    assert wallet.balance == new_balance
+    data = response.json()
+    assert data.get("wallet_id") == str(wallet.id)
+    assert data.get("operation_type") == "WITHDRAW"
+
+
+@pytest.mark.parametrize(
+    "amount",
+    [("0"), ("-1")],
+)
+@pytest.mark.asyncio
+async def test_create_operation_invalid_amount(
+    client,
+    session: AsyncSession,
+    amount: str,
+):
+    wallet = Wallet(email="test@example.com", balance=Decimal("10"))
+    session.add(wallet)
+    await session.commit()
+
+    response = await client.post(
+        f"/api/v1/wallets/{wallet.id}/operation",
+        json={"operation_type": "WITHDRAW", "amount": amount},
+    )
+
+    assert response.status_code == 422
